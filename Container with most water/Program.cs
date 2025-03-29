4135ddfc780057e50c190c82366a7463fca1aae2
Trịnh Nam Siêu Sao <95358751+Sieusao5010@users.noreply.github.com>
2025-03-29 23:12:58 +0700

    Search in Rotated Sorted Array

--- Container with most water/Program.cs
+++ Container with most water/Program.cs
@@ -4,7 +4,7 @@ public class Program
 {
     static void Main()
     {
-        int[] height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+        int[] height = [1, 9, 6, 2, 5, 4, 9, 3, 5];
 
         Solution1 solution1 = new Solution1();
         int result = solution1.MaxArea(height);
---
+++ Search in Rolated Sorted Array/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        int[] nums = { 4, 5, 6, 7, 0, 1, 2, 3 };
+        int target = 5;
+
+        Solution solution = new Solution();
+        int position = solution.Search(nums, target);
+
+        Console.WriteLine("Vị trí của target cần tìm là: " + position);
+    }
+}
