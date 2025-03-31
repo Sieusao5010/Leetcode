8d61b7f27f1fd602aa0d8f1a4e581b4e43ec1f45
Trịnh Nam Siêu Sao <95358751+Sieusao5010@users.noreply.github.com>
2025-03-31 09:22:24 +0700

    Number of Island DFS

---
+++ Number of Islands/DepthFirstSearch.cs
@@ -0,0 +1,38 @@
+﻿
+public class DepthFirstSearch
+{
+    public int NumIslands(char[][] grid)
+    {
+        int count = 0;
+        int rows = grid.Length;
+        int cols = grid[0].Length;
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (grid[i][j] == '1')
+                {
+                    count++;
+                    DFS(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+
+    private void DFS(char[][] grid, int i, int j)
+    {
+        if (i < 0 || i >= grid.Length || j < 0 || j >= grid[0].Length || grid[i][j] == '0')
+        {
+            return;
+        }
+
+        grid[i][j] = '0';
+
+        DFS(grid, i + 1, j); // top
+        DFS(grid, i - 1, j); // bottom
+        DFS(grid, i, j + 1); // left
+        DFS(grid, i, j - 1); // right
+    }
+}
---
+++ Number of Islands/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        char[][] grid = new char[][] {
+                new char[] {'1', '1', '1', '1', '0'},
+                new char[] {'1', '1', '0', '0', '0'},
+                new char[] {'1', '1', '0', '0', '0'},
+                new char[] {'0', '0', '0', '1', '0'}
+        };
+
+        DepthFirstSearch dfs = new DepthFirstSearch();
+        int count1 = dfs.NumIslands(grid);
+        Console.WriteLine("Tìm kiếm số hòn đảo theo DFS: " + count1);
+    }
+}
